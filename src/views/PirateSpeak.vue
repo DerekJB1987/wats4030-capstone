8b058a81782dd5c68d947ac3112c0433619ea9e1
Derek Benak <benak.derek@gmail.com>
2019-02-24 18:16:17 -0800

    added extra vue pages with more translators

---
+++ src/views/PirateSpeak.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="piratespeak">
+    <!-- added router links -->
+    <p>
+      <router-link v-bind:to="'/'">Pirate Speak</router-link>
+    </p>
+
+    <form v-on:submit.prevent="translate">
+      <p>Type your text below to convert to Pirate Speak <input type="text" v-model="text"><button type="submit">Translate</button></p>
+    </form>
+
+    <div v-if= "results && results.contents"><p>{{results.contents.translated}}</p></div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Piratespeak',
+  data () {
+    return {
+      translated: "",
+      text: "",
+      translation: "pirate",
+      results: null,
+      errors: []
+    }
+  },
+
+  methods: {
+    translate: function(){
+      console.log("Why is the rum gone?")
+      axios.get('https://api.funtranslations.com/translate/pirate.json',{
+        params: {
+          text: this.text
+        }
+      })
+      .then(response => {
+        this.results = response.data;
+        console.log(this.results)
+      })
+      .catch(error => {
+        this.errors.push(error);
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.piratespeak {
+  font-size: 1.4rem;
+}
+
+input[type="text"]{
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #333;
+  width: 300px;
+  font-size: 1.4rem;
+  color: #2c3e50;
+  font-weight: 300;
+  background: rgba(0,0,0,0.02);
+  padding: 0.5rem;
+}
+button{
+  background: #333;
+  padding: 0.5rem;
+  font-weight: 300;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 1.4rem;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul.results {
+  list-style-type: none;
+  padding: 0;
+}
+
+.results li {
+  display: inline-block;
+  margin: 10px;
+  border: solid 1px #333;
+  padding: 0.5rem;
+  width: 200px;
+  min-height: 100px;
+  color: #fff;
+  background: rgba(0,0,0,0.7);
+}
+
+a {
+  color: #42b983;
+}
+</style>
