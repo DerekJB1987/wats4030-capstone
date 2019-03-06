7bed6a34ac519c05448d761ab16e26ae80558515
Derek Benak <benak.derek@gmail.com>
2019-03-05 16:55:20 -0800

    fixed navbar

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -1,18 +1,8 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="dark pt-2">
-    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
+  <b-navbar toggleable="lg" type="light" variant="info">
     <b-navbar-brand to="/">Home</b-navbar-brand>
-
-    <b-collapse is-nav id="nav_collapse">
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-navbar-nav fixed="top" active>
-          <b-nav-item to="/aboutderek">About Derek</b-nav-item>
-          <b-nav-item to="/aboutproject">About Project</b-nav-item>
-        </b-navbar-nav>
-      </b-navbar-nav>
-    </b-collapse>
+      <b-navbar-brand to="/aboutderek">About Derek</b-navbar-brand>
+      <b-navbar-brand to="/aboutproject">About Project</b-navbar-brand>
   </b-navbar>
 </template>
 
